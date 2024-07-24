a19632aa67c5d77e7bcd6d2199c65a993e16cbb0
Bongwon <movieid@naver.com>
2024-07-24 13:50:16 +0900

    level1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int result = 0;
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
@@ -17,5 +18,34 @@ public class App {
         // charAt(idx) 문자열 중 입력한 idx 위치에 해당하는 1개의 문자만 반환
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
+
+        switch(operator){
+            case '+':
+                result = num1 + num2;
+                System.out.println("결과 : " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("결과 : " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("결과 : " + result);
+                break;
+
+            case '/':
+                if(num2 == 0)
+                    System.out.println("나눗셈 연산에선 분모(두 번째 정수)에는 0이 입력될 수 없습니다.");
+                else{
+                    result = num1 / num2;
+                    System.out.println("결과 : " + result);
+                }
+                break;
+
+            default:
+                System.out.println("사칙연산 기호가 아닙니다.");
+        }
     }
 }
