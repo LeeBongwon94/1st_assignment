6d31c55f657acf6485ace9645df9cf4cac456ea2
Bongwon <movieid@naver.com>
2024-07-24 12:58:18 +0900

    level1 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,10 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        
+        // sc.next()는 기본적으로 String을 반환하지만, charAt()을 사용함으로
+        // 문자열 중에서 문자 1개만 반환하기에 사용이 가능하다.
+        // charAt(idx) 문자열 중 입력한 idx 위치에 해당하는 1개의 문자만 반환
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
     }
 }
