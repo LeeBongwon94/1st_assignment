f81f7b5e31ac98c2da1162e598d7cbb35d32487e
Robbie <robbie331@>
2024-07-24 11:28:35 +0900

    level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,13 +12,5 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요 : ");
-
-        // sc.next()는 String 문자열을 반환하지만, charAt을 사용함으로 String 문장을
-        // 1글자(Char 형태)를 반환하므로 char형 변수에 저장할 수 있다.
-        // charAt(idx) : 문자열의 idx에 해당하는 문자 반환
-        char operator = sc.next().charAt(0);
-
-        System.out.println("operator = " + operator);
     }
 }
